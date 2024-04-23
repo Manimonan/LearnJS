8289c587d3185dbb942989e27a833408fd9f2fb4
Manimonan <167786934+Manimonan@users.noreply.github.com>
2024-04-23 04:33:43 +0000

    ok

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-console.log( ' this is my fast js coad')
+// let value1 = 'hooghly';
+// let value2 = value1;
+// console.log( value2);
+// value2 = ' barasat';
+// console.log( value2);
+// console.log( value1);
+
+
+
+let userOne = {
+
+    Name: " Manimohan Tarafdar",
+    Id :  "adr1234",
+    Posting: " Barasat"
+
+}
+let userTwo = userOne;
+console.log( userTwo);
+userTwo.Name = " Shilpi Sarkar"
+console.log(userOne.Name);
+console.log(userTwo.Name);
