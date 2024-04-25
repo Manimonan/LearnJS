c0a9da35fcf777be1da66d8dfe0672f730c1b54c
Manimonan <167786934+Manimonan@users.noreply.github.com>
2024-04-25 12:09:00 +0000

    ok

---
+++ Java_Script/Function.js
@@ -0,0 +1,30 @@
+//console.log( "Hellow Warld");
+
+
+// function addTwoNum( num1 , num2){
+//     console.log(num1+num2)
+// }
+// addTwoNum( 3,5)
+// addTwoNum( 10000,452436)
+
+
+// function addTwoNum( num1 , num2){
+
+//    // let Result= num1+ num2;
+//   //  return Result;
+//   return num1 + num2;
+    
+// } 
+// const Result = addTwoNum(4 , 7 );
+// console.log( ` Result is : ${ Result}`)
+
+
+function Userlogin( Username){
+
+    if (Username === undefined){
+         return ` Please enter your username`
+    }
+return ` The login username is: ${ Username}` 
+
+}
+console.log( Userlogin(' Sam'));
--- test.js
+++ test.js
@@ -1,21 +1,2 @@
-// let value1 = 'hooghly';
-// let value2 = value1;
-// console.log( value2);
-// value2 = ' barasat';
-// console.log( value2);
-// console.log( value1);
+// #####......... Strings .........#####
 
-
-
-let userOne = {
-
-    Name: " Manimohan Tarafdar",
-    Id :  "adr1234",
-    Posting: " Barasat"
-
-}
-let userTwo = userOne;
-console.log( userTwo);
-userTwo.Name = " Shilpi Sarkar"
-console.log(userOne.Name);
-console.log(userTwo.Name);
